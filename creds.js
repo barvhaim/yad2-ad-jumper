6f91b141391dbb327e8fc22aa6a8f2e098c30b7b
david knafo <davidkjimdo@gmail.com>
2020-12-04 01:09:21 +0200

    Update creds.js

--- creds.js
+++ creds.js
@@ -1,5 +1,5 @@
 module.exports = {
-    username: 'davidkjimdo@gmail.com',
-    password: 'david1008',
+    username: 'username',
+    password: 'password',
     link: 'https://my.yad2.co.il/newOrder/index.php?action=personalAreaViewDetails&CatID=2&SubCatID=1&OrderID=42808196'
 }
